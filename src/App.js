926642a6b6b3ead4a092396e38aae3a8c0cbce32
raulariias <raularias24685@gmail.com>
2023-03-17 18:09:46 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+
+/* Styles */
+import "./styles/styles.scss";
+
+/* Imgs */
+import patternMobile from './assets/pattern-divider-mobile.svg';
+import patternDesktop from './assets/pattern-divider-desktop.svg';
+import random from './assets/icon-dice.svg';
+
+const App = () => {
+  const API = 'https://api.adviceslip.com/advice'
+  const [number, setNumber] = useState(null)
+  const [advice, setAdvice] = useState('')
+
+  function callApi(api) {
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        setNumber(data.slip.id)
+        setAdvice(data.slip.advice);
+      })
+  } 
+
+  function handdleClick() {
+    return callApi(API);
+  }
+
+  useEffect(() => {
+    callApi(API)
+  }, [])
+
+  return (
+    <>
+      <main className='main'>
+        <div className='cardContainer'>
+          <h1 className='cardContainer__h1'>ADVICE #{number}</h1>
+          <p className='cardContainer__p'>❝{advice}❞</p>
+          <img className='patternMobile' src={patternMobile} alt='Divider' />
+          <img className='patternDesktop' src={patternDesktop} alt='Divider' />
+          <button className='cardContainer__button' onClick={handdleClick}>
+            <img src={random} alt='Random Advice'/>
+          </button>
+        </div>
+      </main>
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://www.frontendmentor.io/profile/rauljariasz">Raul Arias</a>.
+      </footer>
+    </>
+  );
+};
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ],
+        exclude: /\.module\.css$/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
+        ],
+        include: /\.module\.css$/
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      favicon: './src/assets/favicon-32x32.png',
+    }),
+    new MiniCssExtractPlugin()
+  ]
+};
+
+module.exports = config;
